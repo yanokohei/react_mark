91c40c61ad16f7ac7bfbe3fca03980f53d56b646
Yano Kohei <o83184206@gmail.com>
2022-04-15 05:15:05 +0900

    Historyページコンポーネントを親コンポーネントに適用

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,7 @@ import {
   Redirect,
 } from "react-router-dom";
 import { Editor } from "./pages/editor";
+import { History } from "./pages/history";
 
 const GlobalStyle = createGlobalStyle`
   body * {
@@ -24,7 +25,7 @@ const Main = (
         <Editor />
       </Route>
       <Route exact path="/history">
-        <h1>History</h1>
+        <History />
       </Route>
       <Redirect to="/editor" path="*" />
     </Router>
