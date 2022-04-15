3031e1e777cbe4117499b08433fb8f435e67bd83
Yano Kohei <o83184206@gmail.com>
2022-04-16 05:57:40 +0900

    履歴ページにヘッダーコンポーネントを適用

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -1,15 +1,35 @@
 import * as React from "react";
-import { useHistory } from "react-router-dom";
-import { Button } from "../components/button";
+import { Link, useHistory } from "react-router-dom";
 // useHistoryはReactのカスタムフックでhistoryオブジェクト(ブラウザの履歴を扱うAPI)を返します。
+import styled from "styled-components";
+import { Header } from "../components/header";
+
+const HeaderArea = styled.div`
+  position: fixed;
+  right: 0;
+  top: 0;
+  left: 0;
+`;
+
+const Wrapper = styled.div`
+  bottom: 0;
+  left: 0;
+  position: fixed;
+  right: 0;
+  top: 3rem;
+  padding: 0 1rem;
+`;
 
 export const History: React.FC = () => {
-  const history = useHistory();
   // レンダリング関数内で呼び出して戻り値であるhistoryオブジェクトを処理内で使用します。
   return (
     <>
-      <h1>History</h1>
-      <Button onClick={() => history.push("/editor")}>エディタへ戻る</Button>
+      <HeaderArea>
+        <Header title="履歴">
+          <Link to="/editor">エディタに戻る</Link>
+        </Header>
+      </HeaderArea>
+      <Wrapper>TODO: 履歴表示</Wrapper>
     </>
   );
 };
