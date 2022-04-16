c494e6545b41980e099bc99516f74db40ff4a23f
Yano Kohei <o83184206@gmail.com>
2022-04-17 05:55:26 +0900

    履歴リストをヒストリーコンポーネントで読み込んで表示する

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -3,6 +3,11 @@ import { Link, useHistory } from "react-router-dom";
 // useHistoryはReactのカスタムフックでhistoryオブジェクト(ブラウザの履歴を扱うAPI)を返します。
 import styled from "styled-components";
 import { Header } from "../components/header";
+import { getMemos, MemoRecord } from "../indexeddb/memos";
+// 履歴リストを返すメソッドと型をインポート
+
+const { useState, useEffect } = React;
+// JSXを返す関数内に記述すると、処理のたびに処理が実行されてしまうため、useEffectで初期化処理を行う必要があります。
 
 const HeaderArea = styled.div`
   position: fixed;
@@ -20,8 +25,41 @@ const Wrapper = styled.div`
   padding: 0 1rem;
 `;
 
+const Memo = styled.button`
+  display: block;
+  background-color: white;
+  border: 1px solid gray;
+  width: 100%;
+  padding: 1rem;
+  margin: 1rem 0;
+  text-align: left;
+`;
+
+const MemoTitle = styled.div`
+  font-size: 1rem;
+  margin-bottom: 0.5rem;
+`;
+
+const MemoText = styled.div`
+  font-size: 0.85rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+
 export const History: React.FC = () => {
   // レンダリング関数内で呼び出して戻り値であるhistoryオブジェクトを処理内で使用します。
+
+  const [memos, setMemos] = useState<MemoRecord[]>([]);
+  // console.log(memos);
+
+  useEffect(() => {
+    getMemos().then(setMemos);
+  }, []);
+  // useEffectは「副作用(effect)フック」と呼ばれ、レンダリングの後に実行されます。
+  // 第一引数にコールバック関数、履歴リストの取得(非同期処理)したらthenメソッドでuseStateのsetMemosを実行して更新します。
+  // 第二引数に状態が変化するデータを渡します(ここでは空配列)。データに変更があった際に第一引数の関数が発火します。
+
   return (
     <>
       <HeaderArea>
@@ -29,7 +67,15 @@ export const History: React.FC = () => {
           <Link to="/editor">エディタに戻る</Link>
         </Header>
       </HeaderArea>
-      <Wrapper>TODO: 履歴表示</Wrapper>
+      <Wrapper>
+        {memos.map((memo) => (
+          <Memo key={memo.datetime}>
+            <MemoTitle>{memo.title}</MemoTitle>
+            <MemoText>{memo.text}</MemoText>
+          </Memo>
+        ))}
+      </Wrapper>
     </>
   );
 };
+// Memoコンポーネントのkey属性は、配列要素を再描画する際に、変更された要素を特定するための使用するキーで、配列の変更箇所を効率よく判定出来ます。
