99cd2419de35a811a61e4f3369ce9dd6a8392233
Yano Kohei <o83184206@gmail.com>
2022-04-26 05:47:32 +0900

    HTMLの変換にサニタイズのメソッドを使う

--- src/worker/convert_markdown_worker.ts
+++ src/worker/convert_markdown_worker.ts
@@ -1,10 +1,13 @@
 import * as marked from "marked";
+import * as sanitizeHtml from "sanitize-html";
 
 const worker: Worker = self as any; // selfというグローバル変数を代入します。any型で全ての型を許容しています。
 
 worker.addEventListener("message", (event) => {
   const text = event.data;
-  const html = marked(text);
+  const html = sanitizeHtml(marked(text), {
+    allowedTags: [...sanitizeHtml.defaults.allowedTags, "h1", "h2"],
+  });
   worker.postMessage({ html }); // なぜオブジェクトリテラルで囲む必要があるか
   // メインスレッドから受け取ったのテキストデータ（MD）を marked で HTML に変換し、メインスレッドに結果の HTML を返却しています。
   // postMessageでメインスレッドに結果のHTMLを返却しています。
