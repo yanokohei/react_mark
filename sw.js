a0671c9a725928499b0ffd59469f3262996310ec
Yano Kohei <o83184206@gmail.com>
2022-04-28 12:19:42 +0900

    オフライン時にキャッシュを使用する

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+const CacheName = "Cache:v1"; // 任意のキャッシュ名を定義
+
 self.addEventListener("install", (event) => {
   console.log("ServiceWorker install:", event);
 });
@@ -6,16 +8,30 @@ self.addEventListener("activate", (event) => {
   console.log("ServiceWorker activate:", event);
 });
 
+const networkFallingBackToCache = async (request) => {
+  const cache = await caches.open(CacheName);
+  try {
+    const response = await fetch(request); // fetch リクエストを実行してレスポンスを定義
+    await cache.put(request, response.clone()); // レスポンスの内容をコピーしてからキャッシュにレスポンスを保存
+    return response; // レスポンスを呼び出し元に返す
+  } catch (err) {
+    // リクエスト時にエラーが発生した場合に、コンソールにエラーを表示して、キャッシュの内容を返却する処理
+    console.error(err);
+    return cache.match(request);
+  }
+};
 // self はサービスワーカー自身を指します。
 // addEventListener で各イベントにコールバックを登録しています。
 // install activate は前パートで説明したライフサイクルの各イベントを指します。
 
 // ネットワークリクエストに介入
 self.addEventListener("fetch", (event) => {
-  console.log("Fetch to:", event.request.url);
-  event.respondWith(fetch(event.request));
+  // ネットワークリクエストの処理に実行するコールバック処理
+  event.respondWith(networkFallingBackToCache(event.request));
 });
 // addEventListener でfetchイベント時に実行するコールバックを登録しています。
 // fetchイベント時のリクエストURLを出力
-// fetch()の引数には取得したいリソースのパスを渡すことで成功か失敗かに関わらず、リクエストに対する Response に解決する Promise を返します
 // event.respondWith は簡潔に言うと、非同期処理（Promise）の実行終了まで待機してくれるメソッド
+// 通常通りネットワークにアクセスし、エラーが発生したらキャッシュから返します。(networkFallingBackToCache)
+// 通常は最新のリソースを取得し、エラー時のみキャッシュを使用するシンプルな実装です。
+// event.requestにメインスレッドからのリクエスト内容が格納されています。
